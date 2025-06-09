dfd260c754f3b8ef0a344707a45c23417cca3002
baongoc1104 <ltbngoc.work@gmail.com>
2025-06-09 22:54:59 +0700

    sửa tên biến

--- src/algorithms/UCS.py
+++ src/algorithms/UCS.py
@@ -7,20 +7,20 @@ class USC(algos.searching_algorithms):
     def __init__(self):
         super().__init__()
         self.pq = []
-        self.cost_so_far = {}
+        self.current_cost = {}
 
     def start(self):
         super().start()
         start_state = self.maze.initial_state()
-        start_node = node.Node(state=start_state, path_cost=0)
+        start_node = node.Node(state = start_state, path_cost = 0)
         heapq.heappush(self.pq, (start_node.path_cost, start_node))
-        self.cost_so_far[start_state] = 0
+        self.current_cost[start_state] = 0
 
     def step(self):
         if not self.running or not self.pq:
             return
 
-        _, current_node = heapq.heappop(self.pq)
+        _, current_node = heapq.heappop(self.pq) #take out the node with smallest cost
 
         # avoid repetition
         if current_node.state in self.visited_nodes:
@@ -38,17 +38,17 @@ class USC(algos.searching_algorithms):
             return
 
         # neighbour nodes
-        for neighbor_state, action, step_cost in self.maze.get_neighbors(current_node.state):
-            new_cost = current_node.path_cost + step_cost
+        for neighbour_state, action, cost in self.maze.get_neighbors(current_node.state):
+            new_cost = current_node.path_cost + cost
 
-            if (neighbor_state not in self.cost_so_far) or (new_cost < self.cost_so_far[neighbor_state]):
-                self.cost_so_far[neighbor_state] = new_cost
+            if (neighbour_state not in self.current_cost) or (new_cost < self.current_cost[neighbour_state]):
+                self.current_cost[neighbour_state] = new_cost
                 neighbor_node = node.Node(
-                    state=neighbor_state,
-                    parent=current_node,
-                    action=action,
-                    path_cost=new_cost,
-                    depth=current_node.depth + 1
+                    state = neighbour_state,
+                    parent = current_node,
+                    action = action,
+                    path_cost = new_cost,
+                    depth = current_node.depth + 1
                 )
                 heapq.heappush(self.pq, (new_cost, neighbor_node))
        
