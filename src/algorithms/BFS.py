52670c8a3d23a8df35938b27352556f4a915232e
unknown <giabao.150905@gmail.com>
2025-06-15 20:00:21 +0700

    Done my task

---
+++ src/algorithms/BFS.py
@@ -0,0 +1,37 @@
+import algorithms.Node as node
+import algorithms.Algorithms as algos
+import scenes.background as bg
+from collections import deque
+
+
+
+
+class BFS(algos.searching_algorithms):
+    def __init__(self):
+        super().__init__()
+    def start(self):
+        super().start()
+        self.queue = deque([node.Node(self.maze.initial_state())])
+        return None
+
+    def step(self):
+        if not self.queue or not self.running:
+            return
+        current_node = self.queue.popleft()
+        if current_node.state in self.visited_nodes:
+            return
+        bg.pygame.time.delay(50)  # Add a delay to visualize the DFS process
+        self.visited_nodes.add(current_node.state)
+
+        # found goal
+        if self.maze.is_goal_state(current_node.state):
+            self.found_path = True
+            self.running = False
+            self.reconstruct_path(current_node)
+            
+        
+        for neighbor, direction, cost in self.maze.get_neighbors(current_node.state):
+            if neighbor not in self.visited_nodes:
+                self.queue.append(node.Node(neighbor, current_node))
+        
+
